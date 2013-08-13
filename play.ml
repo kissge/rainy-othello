d124b3c286eb473141422c8d4e23fdc16a831958
kissge <y.k@is.s.u-tokyo.ac.jp>
2013-08-13 18:17:35 +0900

    Implemented end stage (alpha-beta)

--- play.ml
+++ play.ml
@@ -61,7 +61,6 @@ let doMove (board, phase) com color =
 	let _  = List.map (fun (ii,jj) -> board.(ii).(jj) <- color) ms in
 	let _  = board.(i).(j) <- color in
 	  (board, phase + 1)
-    | _ -> (board, phase)
 
 let mix xs ys =
   List.concat (List.map (fun x -> List.map (fun y -> (x,y)) ys) xs)
@@ -71,6 +70,95 @@ let valid_moves board color =
   let ls = [1;2;3;4;5;6;7;8] in
   List.filter (is_valid_move board color)
     (mix ls ls)
+
+
+let print_human board color valid_moves =
+  let counter = ref 1 in
+  let moves_sorted = ref [] in
+  (* print_string "\x1b[2J"; *)
+  for j=1 to 8 do 
+    for i=1 to 8 do
+      if List.mem (i, j) valid_moves then
+	( if color = white then Printf.printf "\x1b[37;46m%2x\x1b[m" !counter
+	  else                  Printf.printf "\x1b[30;46m%2x\x1b[m" !counter ;
+	  moves_sorted := (i, j) :: !moves_sorted;
+	  counter := !counter + 1 )
+      else
+	if      board.(i).(j) = white then print_string "\x1b[01;37;42m● \x1b[m"
+	else if board.(i).(j) = black then print_string "\x1b[01;30;42m● \x1b[m"
+	else                               print_string "\x1b[01;30;42m .\x1b[m"
+    done;
+    print_endline ""
+  done;
+  !moves_sorted
+
+let clone arr = Marshal.from_string (Marshal.to_string arr []) 0
+
+
+let count board color =
+  let s = ref 0 in
+  for i=1 to 8 do
+    for j=1 to 8 do
+      if board.(i).(j) = color then s := !s + 1
+    done
+  done;
+  !s
+
+let search_endstage (board, phase) color =
+  let mycolor = color in
+  let enemycolor = opposite_color color in
+  let rec search_me board endflag alpha beta =
+    match valid_moves board mycolor with
+      [] ->
+	if endflag then
+	  count board mycolor
+	else
+	  search_enemy board true alpha beta
+    | ms ->
+      let rec search ms alpha =
+	match ms with
+	  [] -> alpha
+	| (i, j)::ms -> let (nextboard, _) = doMove ((clone board), 0) (Mv (i, j)) mycolor in
+			let alpha = max alpha (search_enemy nextboard false alpha beta) in
+			if alpha >= beta then beta else search ms alpha
+      in
+      search ms alpha
+  and search_enemy board endflag alpha beta =
+    match valid_moves board enemycolor with
+      [] ->
+	if endflag then
+	  count board mycolor
+	else
+	  search_me board true alpha beta
+    | ms ->
+      let rec search ms beta =
+	match ms with
+	  [] -> beta
+	| (i, j)::ms -> let (nextboard, _) = doMove ((clone board), 0) (Mv (i, j)) enemycolor in
+			let beta = min beta (search_me nextboard false alpha beta) in
+			if alpha >= beta then alpha else search ms beta
+      in
+      search ms beta
+  in
+  let rec search_firstmove board ms max_s max_hand =
+    match ms with
+      [] -> (max_s, max_hand)
+    | (i, j)::ms -> let (nextboard, _) = doMove ((clone board), 0) (Mv (i, j)) mycolor in
+		    let try_s = search_enemy nextboard false (-999) 999 in
+		    if max_s <= try_s then
+		      search_firstmove board ms try_s (Mv (i, j))
+		    else
+		      search_firstmove board ms max_s max_hand
+  in
+  let vm = valid_moves board mycolor in
+  let _ = print_human board mycolor vm in
+  print_endline "[END STAGE] Computing all possible moves...";
+  let (max_s, max_hand) = search_firstmove board vm (-1) Pass
+  in
+  print_string "Expectation: ";
+  print_int max_s;
+  print_endline "";
+  max_hand
 
 
 let search_priority board color =
@@ -98,20 +186,13 @@ let play (board, phase) color =
   print_string "Phase: ";
   print_int phase;
   print_endline "";
+  (* print_int ((count board black) + (count board white));*)
   if phase = 4 then
     Mv (6, 5)
+  else if phase > 48 then
+    search_endstage (board, phase) color
   else
     search_priority board color
-
-let count board color =
-  let s = ref 0 in
-    for i=1 to 8 do
-      for j=1 to 8 do
-        if board.(i).(j) = color then s := !s + 1
-      done
-    done;
-    !s
-
 
 let print_board board =
   print_endline " |A B C D E F G H ";
@@ -139,4 +220,3 @@ let report_result board =
     print_string "Black: "; print_endline (string_of_int bc);
     print_string "White: "; print_endline (string_of_int wc);
     print_board board
-
